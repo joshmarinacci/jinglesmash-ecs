0d8316d912598e81a72393ba4214f7a053061466
Josh Marinacci <joshua@marinacci.org>
2019-08-03 15:21:58 -0700

    code cleanup

--- levels.js
+++ levels.js
@@ -2,7 +2,7 @@ import {System} from "./node_modules/ecsy/build/ecsy.module.js"
 import {Consts, toRad} from './common.js'
 import {Block, PhysicsFloor} from './physics.js'
 import {ThreeScene} from './three.js'
-import {Mesh, MeshLambertMaterial, PlaneGeometry} from "./node_modules/three/build/three.module.js"
+import {Mesh, MeshLambertMaterial, PlaneGeometry, Vector3} from "./node_modules/three/build/three.module.js"
 
 function pickOne(arr) {
     return arr[Math.floor(Math.random()*arr.length)]
@@ -19,7 +19,7 @@ export class LevelInfo {
         this.ballType = pickOneValue(Consts.BALL_TYPES)
         this.wallFriction = null
         this.wallRestitution = null
-        // this.gravity = g
+        this.gravity = new Vector3()
         this.hasGravity = true
         this.roomType = 'roomType'
     }
@@ -58,59 +58,43 @@ export  class LevelLoaderSystem extends System {
     }
 
     loadFromJSON(doc,level) {
-        console.log("loading level",doc)
         const sc = this.queries.three[0].getComponent(ThreeScene)
-        // level.blocks.forEach(b => {
-        //     this.group.remove(b.getObject3D())
-        //     world.removeBody(b.body)
-        // })
         level.blocks = []
         const newBlocks = doc.data.blocks.map(b => {
-            // console.log("adding block",b)
             const block = this.world.createEntity()
             block.addComponent(Block)
             const b2 = block.getMutableComponent(Block)
-            // console.log(sc)
             sc.scene.add(b2.obj)
-            // const b2 = this.makeBlock()
-            b2.frozen = true
-            // const p = b.position
             b2.set('position',b.position)
-            // b2.positionSet(p.x,p.y,p.z)
             b2.set('width',b.size.width)
             b2.set('height',b.size.height)
             b2.set('depth',b.size.depth)
             b2.set('rotation',b.rotation)
             b2.set('physicstype',b.physicstype)
-            // b2.frozen = false
-            // b2.rebuildGeometry()
             return block
         })
 
-        // level.ballRadius = doc.data.ballRadius
-        // if(!doc.data.ballRadius) level.ballRadius = 0.25
-        // this.ballMass = doc.data.ballMass
-        // if(!doc.data.ballMass) this.ballMass = 5
-
-        // this.ballType = BALL_TYPES[pickOne(Object.keys(BALL_TYPES))]
-
-        // if(typeof doc.data.wallFriction !== 'undefined') {
-        //     this.wallFriction = doc.data.wallFriction
-        //     this.wallRestitution = doc.data.wallRestitution
+        if(!doc.data.ballRadius) level.ballRadius = 0.25
+        if(!doc.data.ballMass) level.ballMass = 5
+        if(typeof doc.data.wallFriction !== 'undefined') {
+            console.log("wall friction",doc.data.wallFriction)
+            console.log("wall restitution",doc.data.wallRestitution)
+            level.wallFriction = doc.data.wallFriction
+            level.wallRestitution = doc.data.wallRestitution
         //     this.rebuildWallMaterial()
-        // }
+        }
 
-        // if(typeof doc.data.gravity !== 'undefined') {
-        //     const g = doc.data.gravity
-        //     world.gravity.set(g.x,g.y,g.z)
-        // }
-        // if(typeof doc.data.hasGravity !== 'undefined') {
-        //     this.hasGravity = doc.data.hasGravity
-        // }
+        if(typeof doc.data.gravity !== 'undefined') {
+            const g = doc.data.gravity
+            console.log("desired gravity",g)
+            level.gravity.copy(g)
+        }
+        if(typeof doc.data.hasGravity !== 'undefined') {
+            level.hasGravity = doc.data.hasGravity
+        }
         if(typeof doc.data.roomType !== 'undefined') {
             level.roomType = doc.data.roomType
         }
-        //
 
         if(level.roomType === Consts.ROOM_TYPES.FLOOR) {
             this.startFloorRoom(level)
@@ -119,16 +103,6 @@ export  class LevelLoaderSystem extends System {
 
     }
     startFloorRoom() {
-        /*
-        //add floor
-        const floorBody = new CANNON.Body({
-            mass: 0 // mass == 0 makes the body static
-        });
-        floorBody.quaternion.setFromAxisAngle(new CANNON.Vec3(1,0,0),-Math.PI/2);
-        floorBody.addShape(new CANNON.Plane());
-        this.getWorld().addBody(floorBody);
-        floorBody.jtype = BLOCK_TYPES.FLOOR
-         */
         const floorObj = new Mesh(
             new PlaneGeometry(100,100,32,32),
             new MeshLambertMaterial({color:Consts.FLOOR_COLOR})
@@ -137,13 +111,8 @@ export  class LevelLoaderSystem extends System {
         floorObj.rotation.x = toRad(-90)
         const sc = this.queries.three[0].getComponent(ThreeScene)
         sc.scene.add(floorObj)
-
         const floor = this.world.createEntity()
         floor.addComponent(PhysicsFloor)
-
-        // floorBody.userData = {obj:floorObj}
-        // floorBody.userData.skipRaycast = true
-        // this.floors.push(floorBody)
     }
 }
 
