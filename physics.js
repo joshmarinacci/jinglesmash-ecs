485fb96b6f68d8c846d379f8c8d32f7fb4ebb3b6
Josh Marinacci <joshua@marinacci.org>
2019-08-12 20:06:40 -0700

    refactor physics to have more subroutines for each component's lifecycle make sure to remove event handlers from physics objects

--- physics.js
+++ physics.js
@@ -9,32 +9,20 @@ import {Vector3} from "./node_modules/three/build/three.module.js"
 const wallMaterial = new CANNON.Material()
 const ballMaterial = new CANNON.Material()
 
-export class PhysicsBall {
-    constructor() {
-        this.body = null
-    }
-}
-
+export class PhysicsBall {}
 export class PhysicsFloor {}
 export class PhysicsCubeSide {}
-
-export class PhysicsBlock {
-    constructor() {
-        this.body = null
-    }
-}
+export class PhysicsBlock {}
 
 const fixedTimeStep = 1.0 / 60.0; // seconds
 const maxSubSteps = 3;
 
 export class PhysicsSystem extends System {
     init() {
+        this.ballCollided_handler = this.ballCollided.bind(this)
+        this.blockCollide_handler = this.blockCollided.bind(this)
         this.cannonWorld = new CANNON.World();
         this.cannonWorld.gravity.set(0, -9.82, 0);
-
-
-
-
         return {
             queries: {
                 three: { components: [ThreeScene], },
@@ -86,48 +74,11 @@ export class PhysicsSystem extends System {
     }
     execute(delta) {
         const globals = this.queries.globals[0].getMutableComponent(Globals)
-        this.events.blocks.added.forEach((ent,i) => {
-            const base = ent.getComponent(BaseBlock)
-            const phys = ent.getMutableComponent(PhysicsBlock)
-            let type = CANNON.Body.DYNAMIC
-            if(base.physicsType === Consts.BLOCK_TYPES.WALL) type = CANNON.Body.KINEMATIC
-            phys.body = new CANNON.Body({
-                mass: 1,//kg
-                type: type,
-                position: new CANNON.Vec3(base.position.x,base.position.y,base.position.z),
-                shape: new CANNON.Box(new CANNON.Vec3(base.width/2,base.height/2,base.depth/2)),
-                material: wallMaterial,
-            })
-            phys.body.quaternion.setFromEuler(base.rotation.x,base.rotation.y,base.rotation.z,'XYZ')
 
-            this.cannonWorld.addBody(phys.body)
-            phys.body.addEventListener('collide',(e)=>{
-                const globals = this.queries.globals[0].getMutableComponent(Globals)
-                if(!globals.collisionsActive) return
-                if(Math.abs(e.contact.getImpactVelocityAlongNormal() < 1.0)) return
-                if((e.target === phys.body && base.physicsType === Consts.BLOCK_TYPES.CRYSTAL) ||
-                    (e.body === phys.body && base.physicsType === Consts.BLOCK_TYPES.CRYSTAL)) {
-                    if(Math.abs(e.contact.getImpactVelocityAlongNormal() >= 1.5)) {
-                        ent.removeComponent(BaseBlock)
-                    }
-                }
-            })
-        })
-
+        this.events.blocks.added.forEach((ent,i) => this.addBlock(ent,i))
         if(globals.physicsActive) this.cannonWorld.step(fixedTimeStep, delta, maxSubSteps)
-
-        this.queries.blocks.forEach(ent => {
-            const phys = ent.getMutableComponent(PhysicsBlock)
-            const base = ent.getMutableComponent(BaseBlock)
-            base.position.copy(phys.body.position)
-            base.quaternion.copy(phys.body.quaternion)
-        })
-
-        this.events.blocks.removed.forEach(ent => {
-            const phys = ent.getMutableComponent(PhysicsBlock)
-            this.cannonWorld.removeBody(phys.body)
-            ent.removeComponent(PhysicsBall)
-        })
+        this.queries.blocks.forEach(ent => this.syncBlock(ent))
+        this.events.blocks.removed.forEach(ent => this.removeBlock(ent))
 
         this.events.rooms.added.forEach(ent => {
             const base = ent.getComponent(BaseRoom)
@@ -159,14 +110,15 @@ export class PhysicsSystem extends System {
             floor.body = new CANNON.Body({
                 mass: 0 // mass == 0 makes the body static
             });
+            floor.body.user_entity = ent
             floor.body.quaternion.setFromAxisAngle(new CANNON.Vec3(1,0,0),-Math.PI/2);
             floor.body.addShape(new CANNON.Plane());
             this.cannonWorld.addBody(floor.body);
         })
-
         this.events.cubesides.added.forEach(ent => {
             const side = ent.getMutableComponent(PhysicsCubeSide)
             const floorBody = new CANNON.Body({ mass:0 })
+            floorBody.user_entity = ent
             floorBody.addShape(new CANNON.Plane())
             floorBody.quaternion.setFromAxisAngle(side.axis,side.angle);
             floorBody.position.copy(side.pos)
@@ -174,60 +126,130 @@ export class PhysicsSystem extends System {
             this.cannonWorld.addBody(floorBody);
         })
 
-        this.events.balls.added.forEach(ent => {
-            const level = this.queries.levels[0].getComponent(LevelInfo)
-            globals.timeOfLastShot = performance.now()
+        this.events.balls.added.forEach(ent => this.addBall(ent))
+        this.queries.balls.forEach(ent => this.syncBall(ent))
+        this.events.balls.removed.forEach(ent => this.removeBall(ent))
 
-            const ball = ent.getMutableComponent(PhysicsBall)
-            const base = ent.getComponent(BaseBall)
-
-            const pos = base.position
-            const dir = base.velocity
-
-
-            ball.body = new CANNON.Body({
-                mass: level.ballMass,
-                shape: new CANNON.Sphere(base.radius),
-                position: new CANNON.Vec3(pos.x, pos.y, pos.z),
-                velocity: new CANNON.Vec3(dir.x,dir.y,dir.z),
-                material: ballMaterial,
-            })
-            ball.body.addEventListener('collide',(e)=>{
-                if(e.body.position.y !== 0) {
-                    const parts = this.world.createEntity()
-                    parts.addComponent(ParticlesGroup, {position: e.body.position.clone()})
-                }
-            })
-            this.cannonWorld.addBody(ball.body)
-        })
-
-        this.queries.balls.forEach(ent => {
-            const phys = ent.getMutableComponent(PhysicsBall)
-            const base = ent.getMutableComponent(BaseBall)
-            base.position.copy(phys.body.position)
-            base.quaternion.copy(phys.body.quaternion)
-        })
-
-        this.events.balls.removed.forEach(ent => {
-            const phys = ent.getMutableComponent(PhysicsBall)
-            this.cannonWorld.removeBody(phys.body)
-            ent.removeComponent(PhysicsBall)
-        })
-
-        this.events.levels.added.forEach(ent => {
-            const info = ent.getMutableComponent(LevelInfo)
-            this.rebuildWallMaterial(info)
-            if(info.hasGravity) {
-                this.cannonWorld.gravity.set(0,-9.82,0);
-            } else {
-                this.cannonWorld.gravity.set(0,0,0)
-            }
-        })
+        this.events.levels.added.forEach(ent => this.addLevel(ent))
     }
     rebuildWallMaterial(info) {
         // console.log("rebuilding the wall material",info.wallFriction, info.wallRestitution)
         this.cannonWorld.addContactMaterial(new CANNON.ContactMaterial(wallMaterial,ballMaterial,
             {friction:info.wallFriction, restitution: info.wallRestitution}))
+    }
+
+    addBlock(ent, i) {
+        const base = ent.getComponent(BaseBlock)
+        const phys = ent.getMutableComponent(PhysicsBlock)
+        let type = CANNON.Body.DYNAMIC
+        if(base.physicsType === Consts.BLOCK_TYPES.WALL) type = CANNON.Body.KINEMATIC
+        phys.body = new CANNON.Body({
+            mass: 1,//kg
+            type: type,
+            position: new CANNON.Vec3(base.position.x,base.position.y,base.position.z),
+            shape: new CANNON.Box(new CANNON.Vec3(base.width/2,base.height/2,base.depth/2)),
+            material: wallMaterial,
+        })
+        phys.body.quaternion.setFromEuler(base.rotation.x,base.rotation.y,base.rotation.z,'XYZ')
+        phys.body.addEventListener('collide',this.blockCollide_handler)
+        phys.body.user_entity = ent
+
+        this.cannonWorld.addBody(phys.body)
+
+    }
+
+    syncBlock(ent) {
+        const phys = ent.getMutableComponent(PhysicsBlock)
+        const base = ent.getMutableComponent(BaseBlock)
+        base.position.copy(phys.body.position)
+        base.quaternion.copy(phys.body.quaternion)
+    }
+
+    removeBlock(ent) {
+        const phys = ent.getMutableComponent(PhysicsBlock)
+        phys.body.removeEventListener(this.blockCollide_handler)
+        this.cannonWorld.removeBody(phys.body)
+        ent.removeComponent(PhysicsBall)
+    }
+
+    blockCollided(e){
+        const globals = this.queries.globals[0].getMutableComponent(Globals)
+        if(!globals.collisionsActive) return
+        if(Math.abs(e.contact.getImpactVelocityAlongNormal() < 1.0)) return
+        if(Math.abs(e.contact.getImpactVelocityAlongNormal() < 1.5)) return
+
+        // console.log("collided",e.target,e.body)
+
+        const tent = e.target.user_entity
+        if(tent.hasComponent(BaseBlock)) {
+            const base = tent.getComponent(BaseBlock)
+            if(base.physicsType === Consts.BLOCK_TYPES.CRYSTAL) {
+                tent.removeComponent(BaseBlock)
+            }
+            return
+        }
+        const bent = e.body.user_entity
+        if(bent.hasComponent(BaseBlock)) {
+            const base = bent.getComponent(BaseBlock)
+            if(base.physicsType === Consts.BLOCK_TYPES.CRYSTAL) {
+                bent.removeComponent(BaseBlock)
+            }
+        }
+    }
+
+
+    addBall(ent) {
+        const globals = this.queries.globals[0].getMutableComponent(Globals)
+        const level = this.queries.levels[0].getComponent(LevelInfo)
+        globals.timeOfLastShot = performance.now()
+
+        const phys = ent.getMutableComponent(PhysicsBall)
+        const base = ent.getComponent(BaseBall)
+
+        const pos = base.position
+        const dir = base.velocity
+
+
+        phys.body = new CANNON.Body({
+            mass: level.ballMass,
+            shape: new CANNON.Sphere(base.radius),
+            position: new CANNON.Vec3(pos.x, pos.y, pos.z),
+            velocity: new CANNON.Vec3(dir.x,dir.y,dir.z),
+            material: ballMaterial,
+        })
+        phys.body.user_entity = ent
+        phys.body.addEventListener('collide',this.ballCollided_handler)
+        this.cannonWorld.addBody(phys.body)
+    }
+
+    syncBall(ent) {
+        const phys = ent.getMutableComponent(PhysicsBall)
+        const base = ent.getMutableComponent(BaseBall)
+        base.position.copy(phys.body.position)
+        base.quaternion.copy(phys.body.quaternion)
+    }
+
+    removeBall(ent) {
+        const phys = ent.getMutableComponent(PhysicsBall)
+        phys.body.removeEventListener('collide',this.ballCollided_handler)
+        this.cannonWorld.removeBody(phys.body)
+        ent.removeComponent(PhysicsBall)
+    }
+
+    ballCollided(e) {
+        if(e.body.position.y !== 0) {
+            const parts = this.world.createEntity()
+            parts.addComponent(ParticlesGroup, {position: e.body.position.clone()})
+        }
+    }
+    addLevel(ent) {
+        const info = ent.getMutableComponent(LevelInfo)
+        this.rebuildWallMaterial(info)
+        if(info.hasGravity) {
+            this.cannonWorld.gravity.set(0,-9.82,0);
+        } else {
+            this.cannonWorld.gravity.set(0,0,0)
+        }
     }
 }
 
