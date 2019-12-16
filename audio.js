eef30b19af796a5560eb9c37a4645b4d25ca2b9e
Josh Marinacci <joshua@marinacci.org>
2019-12-16 11:16:01 -0800

    re-enable audio

--- audio.js
+++ audio.js
@@ -1,4 +1,12 @@
+import {
+    AudioLoader,
+    AudioListener,
+    Audio
+} from "./node_modules/three/build/three.module.js"
+
 import {System} from "./node_modules/ecsy/build/ecsy.module.js"
+import {ThreeScene} from './three'
+
 
 export class SoundEffect {
 }
@@ -9,9 +17,16 @@ export class PlaySoundEffect {
 
 export class AudioSystem extends System {
     init() {
-        this.context = new window.AudioContext()
+        // this.context = new window.AudioContext()
         return {
             queries: {
+                three: {
+                    components: [ThreeScene],
+                    events: {
+                        added: {event:'EntityAdded'},
+                        removed: {event:'EntityRemoved'}
+                    }
+                },
                 sounds: {
                     components:[SoundEffect],
                     events: {
@@ -31,8 +46,27 @@ export class AudioSystem extends System {
     }
 
     execute(delta) {
+        this.events.three.added.forEach(ent => {
+            this.listener = new AudioListener()
+            this.loader = new AudioLoader()
+            const core = ent.getMutableComponent(ThreeScene)
+            core.camera.add(this.listener)
+        })
         this.events.sounds.added.forEach(ent => {
-            const sound = ent.getMutableComponent(SoundEffect)
+            const effect = ent.getMutableComponent(SoundEffect)
+            const sound = new Audio(this.listener)
+            effect.sound = sound
+            this.loader.load(effect.src,(buffer) => {
+                sound.setBuffer(buffer)
+                if(effect.loop) {
+                    sound.setLoop(true)
+                }
+                if(effect.autoPlay) {
+                    console.log("starting to play",effect.src)
+                    sound.play()
+                }
+            })
+            /*
             return fetch(sound.src,{responseType:'arraybuffer'})
                 .then(resp => resp.arrayBuffer())
                 .then(arr => {
@@ -44,21 +78,24 @@ export class AudioSystem extends System {
                         this.playSound(sound)
                     }
                 })
+
+             */
         })
         this.events.playing.added.forEach(ent => {
             const sound = ent.getMutableComponent(SoundEffect)
+            sound.sound.play()
             setTimeout(()=>{
                 ent.removeComponent(PlaySoundEffect)
             },5)
-            this.playSound(sound)
+            // this.playSound(sound)
         })
     }
 
-    playSound(sound) {
-        const source = this.context.createBufferSource();
-        source.buffer = sound.data
-        source.connect(this.context.destination);
-        source.start(0)
-        return source
-    }
+    // playSound(sound) {
+    //     const source = this.context.createBufferSource();
+    //     source.buffer = sound.data
+    //     source.connect(this.context.destination);
+    //     source.start(0)
+    //     return source
+    // }
 }
